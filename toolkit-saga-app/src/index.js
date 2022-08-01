d81ffaa410a5b57193a4508984439b42d3875590
Ahmad Akel <ahmad-akel@mgm-tp.com>
2022-08-01 06:40:54 +0200

    Redux Toolkit Setup

--- toolkit-saga-app/src/index.js
+++ toolkit-saga-app/src/index.js
@@ -3,9 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
+import { Provider } from "react-redux";
+import createSagaMiddleware from "redux-saga";
+import { configureStore } from "@reduxjs/toolkit";
+import catsReducer from "./catState";
+
+const saga = createSagaMiddleware();
+const store = configureStore({
+  reducer: {
+    cats: catsReducer,
+  },
+  middleware: [saga],
+});
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
